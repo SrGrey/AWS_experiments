00aa0dd81aeac065ae0dfcafa451905bfa595802
SergioAvargin <sergey@ateneamobility.com>
2024-06-06 11:33:26 +0100

    Updating file from IDE

--- lambda_function.py
+++ lambda_function.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda! charged from Github. Version 0.01')
+        'body': json.dumps('Hello from Lambda! charged from Github. Version 0.10 merging from IDE')
     }
